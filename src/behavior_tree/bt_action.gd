ea5af5a09a76fbced04b92fc539380140e1b6c38
Khusheete <souchet.ferdinand@laposte.net>
2024-06-03 10:04:10 +0200

    Added license text (MIT attribution)

--- src/behavior_tree/bt_action.gd
+++ src/behavior_tree/bt_action.gd
@@ -1,6 +1,26 @@
-@icon("../../assets/icons/bt_action.svg")
+# Copyright (c) 2024 Souchet Ferdinand (@Khusheete)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 ## Tries to execute some action using the `_tick` function.
 ## `_tick` will be executed during the BehaviorTree's process thread.
+@icon("../../assets/icons/bt_action.svg")
 class_name BTAction
 extends "bt_node.gd"
 
